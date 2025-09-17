742dc7598e85cc2334ddf295e266a8f642e97926
Tirth Patel <tirthpatel@Tirths-MacBook-Pro.local>
2025-09-17 20:00:47 +0530

    first commit

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Ensure settings are set for ASGI import on Vercel
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitesphere.settings")
-
-from sitesphere.asgi import application as app  # ASGI callable expected by Vercel
-
-
